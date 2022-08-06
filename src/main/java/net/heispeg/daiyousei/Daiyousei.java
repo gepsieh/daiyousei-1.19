28e02e33f6f02d4c4511280de54345df3e03c944
13867 <314308943@qq.com>
2022-08-06 11:17:21 +0800

    Changes

---
+++ src/main/java/net/heispeg/daiyousei/Daiyousei.java
@@ -0,0 +1,15 @@
+package net.heispeg.daiyousei;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Daiyousei implements ModInitializer {
+	public static final String MOD_ID = "daiyousei";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/heispeg/daiyousei/DaiyouseiClient.java
@@ -0,0 +1,11 @@
+package net.heispeg.daiyousei;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DaiyouseiClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
